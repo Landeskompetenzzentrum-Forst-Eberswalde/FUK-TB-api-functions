c4666923ff80d52e4932ac5b6dcb411f26bf45bb
Gerrit Balindt <gerrit.balindt@gruenecho.de>
2025-02-09 20:36:56 +0100

    clean

--- R/icp_access/basic.R
+++ R/icp_access/basic.R
@@ -17,22 +17,16 @@ library(readr)
 
 load_dot_env(file = paste(getwd(), "/.env", sep = ""))
 
-print(Sys.getenv("ANON_KEY"))
-
 
 # GET DATA
 # About querying: https://docs.postgrest.org/en/v12/references/api/tables_views.html
 response <- GET(
     url = paste(Sys.getenv("ICP_SERVER_URL"), "d_accuracy", sep = ""),
     query = list(
-        "or" = "(code.eq.2,code.eq.3)"
+        "or" = "(code.eq.2)"
     ),
-    encode = "json",
-    verbose(),
-    accept_json(), 
     add_headers('apikey' =  Sys.getenv("ANON_KEY")),
     add_headers('Accept' = 'text/csv')
 )
 #content <- content(response, as = "parsed")
 content <- content(response, type = "text/csv")
-print(content)
